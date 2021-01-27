59fed3c5d3a3ba7621bd88d7fbb78bb13dbf37f7
Jd2020D <jed19of@gmail.com>
2021-01-27 14:55:41 +0200

    first commit

---
+++ ReactJavascript/OOP/Ninja.js
@@ -0,0 +1,24 @@
+class Ninja{
+    constructor(name,health){
+        this.name=name;
+        this.health=health;
+        this.speed=3;
+        this.strength=3;
+    }
+    sayName(){
+        console.log(`${this.name}`);
+    }
+    showStats(){
+        console.log(`${this.name} \nStrenghth: ${this.strength} \nSpeed: ${this.speed} \nHealth: ${this.health} ` );
+    }
+
+    drinkSake(){
+        this.health+=10;
+    }
+
+}
+const ninja1 = new Ninja("newNinja",100);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
