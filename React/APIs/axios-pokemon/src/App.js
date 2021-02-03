5b584bfc92dbf762e839652c7c87b03544f5b64c
Jd2020D <jed19of@gmail.com>
2021-02-04 00:36:19 +0200

    axios,fetch

--- React/APIs/axios-pokemon/src/App.js
+++ React/APIs/axios-pokemon/src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
   const [showPokemons, setShowPokemons] = useState(false);
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
-        .then(response => response.json())
-        .then(response => setPokemons(response.results))
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+        .then(response => response.data)
+        .then(response=> setPokemons(response.results))
       }, []);
 
   return (
